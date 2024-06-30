ff151c87cf298a5a6d2db1f738cb500e4744337a
mateusahschulz <mateus.ahschulz@gmail.com>
2024-06-30 15:20:39 -0300

    adição de informações para aparecer no broker do MQTT.

--- trab-final-redes/trab-final-redes.ino
+++ trab-final-redes/trab-final-redes.ino
@@ -17,6 +17,7 @@ PubSubClient clientMQTT(esp32Client);
 unsigned long lastTimeChecked = 0;
 const char* topic = "topic/ble/devices";
 const long publishBreak = 20000;
+const char* mqttBroker = "192.168.0.223";
 
 BLEScan* pBLEScan;
 
@@ -33,7 +34,12 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
 
   void onResult(BLEAdvertisedDevice device) {
     double deviceDistance = calculateDistanceFromRSSI(device.getRSSI());
-    Serial.println(F((String("Dispositivo: ") + String(device.getAddress().toString().c_str()) + String("\nNome: ") + String(device.getName().c_str()) + String("\nDistância: ") + String(deviceDistance, 4)).c_str()));
+    Serial.println(F((
+        String("Dispositivo: ") + 
+        String(device.getAddress().toString().c_str()) + 
+        String("\nNome: ") + String(device.getName().c_str()) + 
+        String("\nDistância: ") + String(deviceDistance, 4)
+      ).c_str()));
 
     if (device.getRSSI() < referenceRSSI) {
       // Serial.println(advertisedDevice.getRSSI());
@@ -46,7 +52,6 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
 };
 
 void setup() {
-  const char* mqttBroker = "test.mosquitto.org";
   Serial.begin(115200);
   connectWifi();
   clientMQTT.setServer(mqttBroker, 1883);
@@ -59,22 +64,23 @@ void setup() {
 }
 
 void connectWifi() {
-  const char* wifiName = "AHSCHULZ.2G";
-  const char* wifiPassword = "7082D154";
+  const char* wifiName = "Mateus";
+  const char* wifiPassword = "mahs12345";
   WiFi.begin(wifiName, wifiPassword);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
   Serial.println(F("Wi-Fi conectado"));
+  Serial.println("IP ESP32: " + String(WiFi.localIP()));
 }
 
 
 void connectMQTT() {
-  // const char* ClientID = "mqtt_trab_final_redes_02_05_2024";
+  const char* ClientID = "mqttx_4ba90028";
   // const char* usuario = "mqtt_redes";
   // const char* senha = "!T&ste123";
   while (!clientMQTT.connected()) {
-    clientMQTT.connect("");
+    clientMQTT.connect(ClientID);
     Serial.println(F("Conectado ao MQTT"));
   }
 }
@@ -99,8 +105,9 @@ void scanBLE() {
   for (uint32_t i = 0; i < deviceCount; i++) {
     BLEAdvertisedDevice device = foundDevices.getDevice(i);
     double deviceDistance = calculateDistanceFromRSSI(device.getRSSI());
-    // Serial.println(F((String("Dispositivo: ") + String(device.getAddress().toString().c_str()) + String(device.getName().c_str()) + String("\nDistância: ") + String(deviceDistance, 4)).c_str()));
-    publishScanResults(device.getName().c_str(), deviceDistance);
+    Serial.println(F((String("Dispositivo: ") + String(device.getAddress().toString().c_str()) + String(device.getName().c_str()) + String("\nDistância: ") + String(deviceDistance, 4)).c_str()));
+    Serial.println("\n");
+    publishScanResults(device.getName().c_str(), deviceDistance, device.getAddress().toString().c_str());
   }
 }
 
@@ -119,10 +126,16 @@ void publishScanResults(int deviceCount) {
   // }
 }
 
-void publishScanResults(const char* deviceName, double deviceDistance) {
+void publishScanResults(const char* deviceName, double deviceDistance, const char* deviceAdress) {
   // if (millis() - lastTimeChecked > publishBreak) {
   //   clientMQTT.publish(topic, String("Não há dispositivos no alcance!").c_str(), true);
   // } else {
-    clientMQTT.publish(topic, (String("Dispositivo: ") + String(deviceName) + String("\nDistância: ") + String(deviceDistance, 4)).c_str(), true);
+  clientMQTT.publish(topic, (
+      String("IP do ESP32: ") + String(WiFi.localIP()) +
+      String("\nDispositivo: ") + String(deviceName) +
+      String("\nDistancia: ") + String(deviceDistance, 4) +
+      String("\nEndereço Mac: ") + String(deviceAdress) + 
+      String("\nIP do broker: ") + String(mqttBroker) + 
+      String("\nPorta do Broker: 1883")).c_str(), true);
   // }
 } 
